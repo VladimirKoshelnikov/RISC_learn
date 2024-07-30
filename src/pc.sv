ecaade2216ae6a48f290dc723d14c868ca115811
Koshelnikov Vladimir <vkoshelnikov@bk.ru>
2024-07-30 14:35:23 +0300

    Make program counter with ability to change program-line number

--- src/pc.sv
+++ src/pc.sv
@@ -5,11 +5,12 @@ module pc #(
 ) (
     input   bit clk,
     input   bit a_reset_n,
-    output  bit [COUNTER_WIDTH -1 : 0] cmd_address
+    input   bit [COUNTER_WIDTH -1 : 0] cmd_address_next,
+    output  bit [COUNTER_WIDTH -1 : 0] cmd_address_current
 );
 
     always_ff @(posedge clk, negedge a_reset_n) begin
-        cmd_address <= ~a_reset_n ? START_ADDRESS : cmd_address + CMD_WIDTH;
+        cmd_address_current <= ~a_reset_n ? START_ADDRESS : cmd_address_next;
     end
 
 endmodule
--- testbench/pc_tb.sv
+++ testbench/pc_tb.sv
@@ -1,29 +1,38 @@
+`timescale 1ps/1ps
 module pc_tb();
     
     bit clk;
     bit a_reset_n;
-    bit [31:0] cmd_address;
+    bit [31 : 0] cmd_address_next;
+    bit [31 : 0] cmd_address_current;
 
-    pc dut (.clk(clk), .a_reset_n(a_reset_n), .cmd_address(cmd_address));
+    pc dut (.clk(clk), .a_reset_n(a_reset_n), .cmd_address_next(cmd_address_next), .cmd_address_current(cmd_address_current));
 
     initial begin
         clk = 0;
         forever begin
-            #0.5; clk = ~clk;
+            #1; clk = ~clk;
         end
     end
 
 
     initial begin
-        #5;
         a_reset_n = 1'b0;
         #1;
         a_reset_n = 1'b1;
-        #50;
+        #1;
+        cmd_address_next = 4;
+        #2;     cmd_address_next = 8;
+        #2;     cmd_address_next = 12;
+        #2;     cmd_address_next = 16;
+        #2;     cmd_address_next = 20;
+        #2;     cmd_address_next = 24;
+        a_reset_n = 1'b0;
+        #10;
         $stop();
     end
 
     initial begin
-        $monitor("clk: %1b; a_reset_n: %1b, cmd_address %32h",clk, a_reset_n, cmd_address);
+        $monitor("time: %0d; clk: %1b; a_reset_n: %1b, cmd_address_next %32h, cmd_address_current: %32h",$time(), clk, a_reset_n, cmd_address_next, cmd_address_current);
     end
 endmodule
