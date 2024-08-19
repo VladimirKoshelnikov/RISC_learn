60368ed15a7d40d2185a5db4b3be60aff9b585e0
Koshelnikov Vladimir <vkoshelnikov@bk.ru>
2024-08-19 18:48:26 +0300

    Made Refactoring register file

--- src/rf.sv
+++ src/rf.sv
@@ -1,7 +1,7 @@
 module rf #(
-    parameter REGISTER_ADDRESS_WIDTH = 5,
-    parameter REGISTER_ADDRESS_DEPTH = 32,
-    parameter DATA_WIDTH = 32
+    parameter REGISTER_ADDRESS_WIDTH    = 5,
+    parameter REGISTER_ADDRESS_DEPTH    = 32,
+    parameter DATA_WIDTH                = 32
 ) (
     input   bit clk,
     input   bit a_reset_n,
@@ -15,19 +15,19 @@ module rf #(
     output  bit [DATA_WIDTH - 1 : 0] read_data_1,
     output  bit [DATA_WIDTH - 1 : 0] read_data_2,
     
-    input  bit [DATA_WIDTH - 1 : 0] write_data
+    input   bit [DATA_WIDTH - 1 : 0] write_data
 );
 
-    bit [REGISTER_ADDRESS_DEPTH - 1 : 0] [DATA_WIDTH - 1: 0] RAM ;   /*block ram*/
+    bit [REGISTER_ADDRESS_DEPTH - 1 : 0] [DATA_WIDTH - 1: 0] RF_DATA ;   /*block ram*/
 
     always_ff @(posedge clk, negedge a_reset_n) begin
         if (~a_reset_n)  begin
-            RAM <= 'b0;
+            RF_DATA <= 'b0;
         end
         else begin
-            read_data_1 <= RAM[address1];
-            read_data_2 <= RAM[address2];
-            if (we & address3 != 0 ) RAM[address3] <= write_data;
+            read_data_1 <= RF_DATA[address1];
+            read_data_2 <= RF_DATA[address2];
+            if (we & address3 != 0 ) RF_DATA[address3] <= write_data;
         end 
     end
 
